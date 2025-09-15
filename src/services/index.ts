d5e52a4f213bab517c800107742cda8a1458ad7d
nguyenthanhphat1510 <nguyenthanhphat2004.st@gmail.com>
2025-09-15 21:07:29 +0700

    fix

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,3 @@
 export * from './ProductService/productService';
 export * from './AuthService/authService';
 export * from './CartService/cartService';
-
-// Default exports
-export { productService } from './ProductService/productService';
-export { authService } from './AuthService/authService';
-export { cartService } from './CartService/cartService';
